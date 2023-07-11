55e1f112ebb292f2a5d238e0594693a5299128c1
KurenkievTimur <ti-mur99@mail.ru>
2023-07-11 15:33:57 +0600

    decode with unicode table

--- src/main/java/com/kurenkievtimur/Main.java
+++ src/main/java/com/kurenkievtimur/Main.java
@@ -3,16 +3,24 @@ package com.kurenkievtimur;
 import java.util.Scanner;
 
 public class Main {
-    private final static String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String action = scanner.nextLine();
+
         String text = scanner.nextLine();
+
         int shift = scanner.nextInt();
 
-        String encode = encode(text, shift);
-        System.out.println(encode);
+        String cipher = "";
+        if (action.equals("enc")) {
+            cipher = encode(text, shift);
+        } else if (action.equals("dec")) {
+            cipher = decode(text, shift);
+        }
+
+        System.out.println(cipher);
     }
 
     public static String encode(String text, int shift) {
@@ -20,14 +28,18 @@ public class Main {
         char[] charArray = text.toCharArray();
 
         for (Character ch : charArray) {
-            int oldIndex = alphabet.indexOf(ch);
+            builder.append((char) (ch + shift));
+        }
 
-            if (oldIndex == -1) {
-                builder.append(ch);
-            } else {
-                int newIndex = (oldIndex + shift) % 26;
-                builder.append(alphabet.charAt(newIndex));
-            }
+        return builder.toString();
+    }
+
+    public static String decode(String text, int shift) {
+        StringBuilder builder = new StringBuilder();
+        char[] charArray = text.toCharArray();
+
+        for (Character ch : charArray) {
+            builder.append((char) (ch - shift));
         }
 
         return builder.toString();
